450edb51cce9ab9e0f452652a2f632a57eb6afd5
whoppix <whoppix@2cb9e645-c0d4-456c-95cb-819b542c0b60>
2008-12-03 20:54:37 +0000

    testmethod added
    git-svn-id: svn://kekz.net/rattlekekz/trunk@28 2cb9e645-c0d4-456c-95cb-819b542c0b60

--- kekzprotocol.py
+++ kekzprotocol.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+def __init__(self, controller):
+    self.controller = controller
+    print "initialisizing model"
+    
+def sendToServer(self, string):
+    print "sending to server: " + string
+    
