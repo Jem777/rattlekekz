5123b0646217374ceebf56d7ce1d83950c546d1a
jem <jem@2cb9e645-c0d4-456c-95cb-819b542c0b60>
2008-12-07 00:43:41 +0000

    connectionMade added
    git-svn-id: svn://kekz.net/rattlekekz/trunk@42 2cb9e645-c0d4-456c-95cb-819b542c0b60

--- kekzprotocol.py
+++ kekzprotocol.py
@@ -22,7 +22,7 @@ class KekzClient(basic.LineOnlyReceiver):
 
     def __init__(self,Object):
         """Called, when an object of the KekzClient class is created"""
-        self.Controller=Object
+        self.controller=Object
         self.pingAnswer=False
         self.pwhash=None
         self.nickname=""
@@ -109,6 +109,9 @@ class KekzClient(basic.LineOnlyReceiver):
 
 
 # Following Methods are called if the server sends something
+    def connectionMade(self):
+        """It doesn't fit the naming pattern, i know"""
+        self.controller.gotConnection()
 
     def lineReceived(self,data):
         nummer=data[:3]
@@ -123,35 +126,35 @@ class KekzClient(basic.LineOnlyReceiver):
 
     def kekzCode000(self,data):
         self.pwhash=data
-        self.Controller.receivedHandshake()
+        self.controller.receivedHandshake()
 
     def kekzCode010(self,data):
         """Creats an array of rooms received """
         rooms=json.JSONDecoder().decode(data) #decoder.decode(data)
-        self.Controller.receivedRooms(rooms)
+        self.controller.receivedRooms(rooms)
     
     def kekzCode020(self,data):
         userdata=json.JSONDecoder().decode(data)
         nick,status,room=userdata["nick"],userdata["status"],userdata["room"]
         self.nickname=nick
-        self.Controller.successLogin(nick,status,room)
+        self.controller.successLogin(nick,status,room)
 
     def kekzCode030(self,data):
-        self.Controller.successRegister()
+        self.controller.successRegister()
 
     def kekzCode031(self,data):
-        self.Controller.successNewPassword()
+        self.controller.successNewPassword()
 
     def kekzCode040(self,data):
         dic=json.JSONDecoder().decode(data)
         name,ort,homepage,hobbies=dic["name"],dic["ort"],dic["homepage"],dic["hobbies"]
-        self.Controller.receivedProfile(name,ort,homepage,hobbies)
+        self.controller.receivedProfile(name,ort,homepage,hobbies)
 
     def kekzCode041(self,data):
-        self.Controller.successNewProfile()
+        self.controller.successNewProfile()
 
     def kekzCode088(self,data):
-        self.Controller.receivedPing(self.lastPing-time.time())
+        self.controller.receivedPing(self.lastPing-time.time())
         self.pingAnswer=False
 
     def kekzCode100(self,data):
@@ -159,54 +162,54 @@ class KekzClient(basic.LineOnlyReceiver):
         channel,nick=foo[0],foo[1]
         msg=" ".join(foo[2:])
         if not msg: return
-        self.Controller.receivedMsg(nick,channel,msg)
+        self.controller.receivedMsg(nick,channel,msg)
 
     def kekzCode102(self,data):
         foo=data.split(" ")
         nick=foo[0]
         msg=" ".join(foo[1:])
-        self.Controller.receivedMsg(nick,self.Nickname,msg)
+        self.controller.receivedMsg(nick,self.Nickname,msg)
 
     def kekzCode103(self,data):
         foo=data.split(" ")
         nick=foo[0]
         msg=" ".join(foo[1:])
-        self.Controller.privMsg(nick,msg)
+        self.controller.privMsg(nick,msg)
 
     def kekzCode109(self,data):
         foo=data.split(" ")
         nick=foo[0]
         msg=" ".join(foo[1:])
-        self.Controller.botMsg(nick,msg)
+        self.controller.botMsg(nick,msg)
     
     def kekzCode110(self,data):
         pass
 
     def kekzCode901(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCode920(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCode921(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCode930(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCode940(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCode941(self,data):
         dic=json.JSONDecoder().decode(data)
         id,msg=dic["id"],dic["msg"]
-        self.Controller.Error(id+" "+msg)
+        self.controller.Error(id+" "+msg)
 
     def kekzCode988(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
     def kekzCodeUnknown(self,data):
-        self.Controller.Error(data)
+        self.controller.Error(data)
 
 
 
@@ -218,7 +221,7 @@ class KekzFactory(protocol.ClientFactory):
     def __init__(self,Object):
         self.Object=Object
     def clientConnectionLost(self, connector, reason):
-        self.Object.Controller.Error("Verbindung verloren: "+reason)
+        self.Object.controller.Error("Verbindung verloren: "+reason)
     def clientConnectionFailed(self, connector, reason):
-        self.Object.Controller.Error("Verbindung kann nicht hergestellt werden: "+reason)
+        self.Object.controller.Error("Verbindung kann nicht hergestellt werden: "+reason)
         reactor.stop()
