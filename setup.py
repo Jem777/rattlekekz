d710ab657f526bd9bc742c1f937f5d0fb1206e69
jem <jem@2cb9e645-c0d4-456c-95cb-819b542c0b60>
2009-02-06 16:26:23 +0000

    added author and url to setup.py added README.txt it's empty though
    git-svn-id: svn://kekz.net/rattlekekz/trunk@372 2cb9e645-c0d4-456c-95cb-819b542c0b60

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(name='Keckz',
       version='0.1',
+      author="KECKz Team",
+      author_email="egg@spam.de",
       packages=['KECKz','KECKz.core','KECKz.cliView'],
-      data_files=[('/bin',['bin/keckz'])],
-      requires=['twisted(>=8.1.0)','urwid','json','OpenSSL']
-      #package_dir = {'keckz': 'lib.keckz'},
-      #package_data = {'keckz':['licence/']},
-      #libraries=['twisted', 'urwid'],
+      scripts=['bin/keckz'],
+      requires=['twisted(>=8.1.0)','urwid','json','OpenSSL'],
+      url="http://kekz.net/"
      )
