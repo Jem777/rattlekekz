d5d907ff2f11019e75be44331c4046880803bb98
Christian Scharkus <cscharkus@googlemail.com>
2010-02-14 12:22:25 +0100

    adjusted some values in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 from glob import glob
 
 setup(name='rattlekekz',
-      version='0.1',
-      author="rattlekekz Team",
-      author_email="egg@spam.de",
+      version='20100214',
+      author="Moritz Doll, Christian Scharkus",
+      author_email="mail [dot] sensenmann [at] gmail [dot] com",
       packages=['rattlekekz','rattlekekz.core','rattlekekz.cliView'],
       scripts=['bin/rattlekekz'],
       requires=['twisted(>=8.1.0)','urwid','simplejson','OpenSSL'],
-      url="http://kekz.net/",
+      url="http://github.com/Jem777/rattlekekz",
       license="GPL v3 or higher",
       data_files=[('share/emoticons/rattlekekz',glob("rattlekekz/emoticons/*.png")),
         ('share/emoticons/rattlekekz/kekz',glob("rattlekekz/emoticons/kekz/*.png"))]
