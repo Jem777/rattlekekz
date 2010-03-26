7fdf633d3a123459c0e93c79619121c2911dde07
Christian Scharkus <cscharkus@googlemail.com>
2010-03-26 15:11:58 +0100

    adjusted setup.py for new deb

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 from glob import glob
 
 setup(name='rattlekekz',
-      version='20100315',
+      version='20100326',
       author="Moritz Doll, Christian Scharkus",
       author_email="look@github.com",
       packages=['rattlekekz','rattlekekz.core','rattlekekz.cliView'],
       scripts=['bin/rattlekekz'],
-      description="python-based commandline client for the kekz.net chat",
+      description="python-based commandline client for the crumbled chat",
       url="http://github.com/Jem777/rattlekekz",
       license="GPL v3 or higher",
       data_files=[('share/emoticons/rattlekekz',glob("rattlekekz/emoticons/*.png"))]
